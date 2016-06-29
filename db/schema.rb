0c57de8b48e5598684b7c476d601731ba8dbf0d7
Sukoreno Mukti <sukorenomw@gmail.com>
2016-06-29 12:20:14 +0700

    added field question and answer to question model table

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@
 ActiveRecord::Schema.define(version: 20160629040233) do
 
   create_table "questions", force: :cascade do |t|
+    t.string   "question"
+    t.string   "answer"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
